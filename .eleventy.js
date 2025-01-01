90baef52c69e590ae7b343509b781cad643e93aa
Mihir Patankar <mpatankar06@gmail.com>
2025-01-01 12:07:12 -0500

    add image passthrough and tweak styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 import CleanCSS from "clean-css";
 
 export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("images");
+
   eleventyConfig.addFilter("cssmin", (code) => {
     return new CleanCSS({}).minify(code).styles;
   });
